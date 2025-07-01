3e20b0896ba28c989c38552734f7b5076dde924a
Danielvalim <daniel.valim.silva@escola.pr.gov.br>
2025-07-01 10:37:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual a cor da escrita na bandeira do brasil?',
+    'Verde'
+)
+
+criaCartao(
+    'Jogos',
+    'Em que ano o ps5 foi lançado?',
+    'Foi lançado em 2020'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantas cores tem na bandeira do Brasil?',
+    '4 cores'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
